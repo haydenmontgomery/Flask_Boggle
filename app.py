c5319644e56ca7e3254b87af6793f7268ec3bad8
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-04-09 11:42:28 -0400

    Added requirements.txt, setup flask and debug on app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc123" # Very secret
+app.debug = True
+
+toolbar = DebugToolbarExtension(app)
 boggle_game = Boggle()
