df692d0c519a88f1a5a1faf83d17cd9f9a4339e4
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-04-10 11:56:50 -0400

    Added home page which displays the board. Changed CSS to have a matrix of buttons

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+from flask import Flask, request, render_template, redirect, session, make_response
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, session
+
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "abc123" # Very secret
+app.config['SECRET_KEY'] = 'abc123' # Very secret
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.debug = True
+
 
 toolbar = DebugToolbarExtension(app)
 boggle_game = Boggle()
 
+
+
+@app.route('/')
+def home_page():
+    board = boggle_game.make_board()
+    session['boggle_board'] = board
+    return render_template('home.html')
