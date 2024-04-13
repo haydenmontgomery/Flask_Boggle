e69f867bb05e4bd9ae40fcf386ec1bae33521b5b
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-04-13 13:34:29 -0400

    Added high score and number of plays tracking. Finishing touches

--- static/app.js
+++ static/app.js
@@ -3,30 +3,21 @@ let timeLeft = 60;
 let canGuess = true;
 let words = new Set();
 $(document).ready(function() {
-    console.log('readybbbbb');
+    $('#guess').focus();
     $('#guess-form').on('submit', async function(e) {
-        console.log('submit to me')
         e.preventDefault();
-        const guess = $('#guess').val();
-        if (words.has(guess)){
-            $('#response').text(`${guess} is already guessed`);
-            return;
-        }
-
-        words.add(guess)
-        const res = await axios.get("/guess", { params: { guess: guess }});
-        if (canGuess == true){  
-            if (res.data.result === "not-word") {
-                $('#response').text(`${guess} is not a word`);
-            } else if (res.data.result === "not-on-board") {
-                $('#response').text(`${guess} is not on this board`);
-            } else {
-                $('#response').text(`${guess} added`);
-                const value = guess.length;
-                setScore(value);
+        if(canGuess){
+            const guess = $('#guess').val();
+            if (words.has(guess)){
+                $('#response').text(`${guess} is already guessed`);
+                return;
             }
+
+            words.add(guess)
+            const res = await axios.get("/guess", { params: { guess: guess }});
+            changeDisplay(res.data.result, guess);
+            $('#guess').val('');
         }
-        $('#guess').val('');
     });
 
     time_counter = setInterval(() => {
@@ -54,5 +45,24 @@ function showTime() {
 }
 
 async function highScore(){
-    const res = await axios.post("/high-score", { score: score });
+    const res = await axios.post("/high_score", { score: score });
+    if (res.data.newRecord){
+        $('#high-score').text(`High Score: ${score}`);
+        $('#response').text("You got a new High Score!!");
+    } else {
+        $('#response').text("Keep trying to get a High Score!");
+    }
+}
+
+async function changeDisplay(result, guess){
+    console.log(result)
+    if (result === "not-word") {
+        $('#response').text(`${guess} is not a word`);
+    } else if (result === "not-on-board") {
+        $('#response').text(`${guess} is not on this board`);
+    } else {
+        $('#response').text(`${guess} added`);
+        const value = guess.length;
+        setScore(value);
+    }
 }
